ccd37151d7a8cd10bfe9531f1b4b9937e6302773
Hong-Kai-Jyun <s99101219@stu.edu.tw>
2020-02-03 21:39:14 +0800

    .

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,11 @@
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<!-- Responsive meta tag -->
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Required meta tags 使用Veiwport 設定手機網頁的螢幕解析度-->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+
+      <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"><!--匯入bootstrap-->
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script><!--匯入jQuery-->
+      <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script><!--匯入bootstrap javascript-->
+    
---
+++ resources/views/partials/mynav.blade.php
@@ -0,0 +1,71 @@
+<nav class="navbar navbar-expand-lg navbar-light navbar-default">
+  <div class="container">
+    <a href="{{ url('/') }}" class="navbar-brand">
+      SYSTEM
+    </a>
+    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a href="{{ action('mySystemController@index') }}" class="nav-link">
+          LIST
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!--menu開始-->  
+<div class="nav navbar-default col-sm-auto"><!-- style="width:30%"navbar-fixed-bottom-->
+
+    <div class="col-sm-1"></div><!--空白1-->
+
+    <div class="nav navbar-default col-sm-auto">
+    <li><a data-toggle="tab" href="#menu2">首頁</a></li>
+	</div>
+	
+    <tr class="nav navbar-nav navbar-right nav-tabs">
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown">我的訂單</a>
+        <ul class="dropdown-menu">
+           <li><a href="#">新增訂單</a></li>
+           <li><a href="#">未結訂單清單</a></li>
+           <li><a href="#">訂單查詢</a></li>
+        </ul>
+      </li>
+	  
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" >庫存</a>
+        <ul class="dropdown-menu">
+           <li><a href="#">待入庫清單</a></li>
+           <li><a href="#">現有庫存清單</a></li>
+           <li><a href="#">歷史庫存查詢</a></li>
+        </ul>
+      </li>
+	  
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" >客戶訂單</a>
+        <ul class="dropdown-menu">
+           <li><a href="#">新增訂單</a></li>
+           <li><a href="#">客戶訂單狀態</a></li>
+           <li><a href="#">查詢歷史交易</a></li>
+        </ul>
+      </li>
+	  
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" >顧客管理</a>
+        <ul class="dropdown-menu">
+           <li><a href="#">新增客戶</a></li>
+           <li><a href="#">客戶資料查詢</a></li>
+        </ul>
+      </li>
+	  
+      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" >供應商管理</a>
+        <ul class="dropdown-menu">
+           <li><a href="#">新增供應商</a></li>
+           <li><a href="#">查詢供應商</a></li>
+        </ul>
+      </li>
+	  
+	  <!--
+       <li><a data-toggle="tab" href="#"><span class="glyphicon glyphicon-log-in"></span> Log in</a></li>
+       <li><a data-toggle="tab" href="#">Log out</a></li>
+       <li><a data-toggle="tab" href="#"><span class="glyphicon glyphicon-user"></span> Admin</a></li>
+      -->    
+	</tr>
+    
+</div>
+<!--menu結束-->
